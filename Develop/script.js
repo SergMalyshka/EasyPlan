be506138646dc0ad655ba57b3cbcc338b9b667e6
Sergei Malyshka <104466946+SergMalyshka@users.noreply.github.com>
2024-01-19 19:34:34 -0500

    comments added
    added comments, changed script and style paths to function after coderestructure

--- Develop/script.js
+++ Develop/script.js
@@ -2,9 +2,13 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  //grab the current time
   var timeNow = dayjs();
+
+  //display current date on top of the screen
   $('#currentDay').html("Today is " + timeNow.format('dddd, MMMM D, YYYY'))
 
+  //traverse through each time-block and render existing tasks if present
   $('.time-block').each(function () {
     var currentHour = idParse((this).id);
     var timeSlot = $(this).children('.hour')[0].textContent;
@@ -13,6 +17,9 @@ $(function () {
     if (task !== null) {
       $(this).children('.description').html(task)
     }
+
+    //Add classes based on whether current time slot is in the past present or future
+
     if (currentHour < parseInt(timeNow.hour())) {
       $(this).addClass('past')
     } else if (currentHour === parseInt(timeNow.hour())) {
@@ -22,7 +29,7 @@ $(function () {
     }
   })
 
-
+  //add event listeners to each button, save each task if present into local storage
   $('.saveBtn').each(function() {
     $(this).on("click", function() {
       var timeSlot = $(this).siblings('.hour')[0].textContent;
@@ -31,12 +38,14 @@ $(function () {
     })
   })
 
+  //save task to local storage
   function saveToLocal(timeSlot, description) {
     var task = JSON.parse(localStorage.getItem(timeSlot));
     task = description;
     localStorage.setItem(timeSlot, JSON.stringify(task));
   }
 
+  //helper method parsing div id into a comparable hour element
   function idParse(stringId) {
     stringId = stringId.replace('hour-', '');
     return parseInt(stringId, 10);
